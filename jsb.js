4fabed1aa61d7e098969cd9e42ada0967703aa89
Hamed Qaderi <hamedghaderii@gmail.com>
2014-02-17 10:05:04 +0330

    "form is not stable"

---
+++ jsb.js
@@ -0,0 +1,36 @@
+window.onclick = cal;
+
+function cal() {
+    if ( document.getElementById ) {
+        var oShow = document.getElementById('show');
+        if ( oShow ) {
+                 oShow.onclick = calculateDistance;
+         }
+    }
+}
+
+function calculateDistance() {
+    var oPlanet = document.getElementById("planet-name").value;
+    var oDistance = document.getElementById("distance");
+    var oDiameter = document.getElementById("diameter");
+
+    if ( oPlanet == "mercury" ) {
+        oDistance.value.write("36 milion miles");
+        oDiameter.value.write("3100 miles");
+        exit();
+    }
+    else if ( oPlanet == "venus" ) {
+        oDistance.value = "67 milion miles";
+        oDiameter.value = "7700 miles";
+    }
+    else if ( oPlanet == "earth" ) {
+        oDistance.value = "93 milion miles";
+        oDiameter.value = "7920 miles";
+    }
+    else if ( oPlanet == "mars" ) {
+        oDistance.value = "141 milion miles";
+        oDiameter.value = "4200 miles";
+    }
+}
+
+
